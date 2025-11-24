59db2f89216fc52fde2ca0dacbcec0302f832bb4
lucasvrm <lucasvrm@gmail.com>
2025-11-24 17:59:15 -0300

    Retirando dependência do GHSpark

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import "@github/spark/spark"
 
 import App from './App.tsx'
 import ErrorFallback from './ErrorFallback.tsx'
